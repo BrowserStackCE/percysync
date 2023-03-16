a5b361100d44716c4c5409ec1f6c224462ffc200
maxmattone <max.m@browserstack.com>
2023-03-16 16:41:43 +0000

    default value based on media build time

--- index.js
+++ index.js
@@ -3,23 +3,34 @@ const { spawn } = require('node:child_process');
 const { program } = require('commander');
 var request = require('sync-request');
 var waitUntil = require('wait-until');
+const { finished } = require('node:stream');
 
 program
-    .option('-t, --timeout <number>', 'percysync timeout (seconds)', 20)
-    .option('-i, --idle <number>', 'minimum amount of time to wait before stating to poll the Percy API (seconds)', 10)
+    .option('-t, --timeout <number>', 'percysync timeout (seconds)')
+    .option('-i, --idle <number>', 'minimum amount of time to wait before stating to poll the Percy API (seconds)')
 
 program.parse();
 
 const options = program.opts();
 const command = spawn(program.args[0], program.args.slice(1));
-const regex = /\[percy\] Finalized build #\d+: https:\/\/percy\.io\/(?<project_id>\w+)\/(.+)\/builds\/(?<build_id>\d+)/gm;
+const regex = /\[percy\] Finalized build #\d+: https:\/\/percy\.io\/(\w+)\/(?<project_slug>.+)\/builds\/(?<build_id>\d+)/gm;
 var build_id = false;
+var project_slug = false;
+
+
+const median = arr => {
+    const mid = Math.floor(arr.length / 2),
+      nums = [...arr].sort((a, b) => a - b);
+    return arr.length % 2 !== 0 ? nums[mid] : (nums[mid - 1] + nums[mid]) / 2;
+  };
+
 command.stdout.on('data', (data) => {
     console.log(data.toString());
     let m;
     if(m = regex.exec(data.toString()))
     {
         build_id = m['groups']['build_id']
+        project_slug = m['groups']['project_slug']
     }
 });
 
@@ -34,6 +45,30 @@ command.on(
     {
         var build_status
         console.log("[percysync] Waiting for Percy to finish the visual report");
+        if(!options['idle'])
+        {
+            var res = request('GET', 'https://percy.io/api/v1/projects?project_slug='+ project_slug, {
+                headers: { 'Authorization': "Token "+ process.env["PERCY_TOKEN"]},
+            });
+            var project_id = JSON.parse(res.getBody('utf8'))["data"]["id"]
+            var res = request('GET', 'https://percy.io/api/v1/builds?project_id='+ project_id+ "&page\[limit\]=50", {
+                headers: { 'Authorization': "Token "+ process.env["PERCY_TOKEN"]},
+            });
+        
+            var finished_builds = JSON.parse(res.getBody('utf8'))["data"].filter(aBuild => aBuild['attributes']['finished-at'])
+            if(finished_builds)
+            {
+                var build_durations = finished_builds.map(aBuild => 
+                    {
+                        return Date.parse(aBuild['attributes']['finished-at']) - Date.parse(aBuild['attributes']['created-at'])
+                    });
+                options['idle'] = median(build_durations)*0.9 / 1000.
+                options['timeout'] = options['idle'] + 60.
+                console.log("[percysync] We expect the build to take around " + options['idle'] + " seconds");
+            }
+        }
+        options['idle'] = options['idle']?options['idle']:30
+        options['timeout'] = options['timeout']?options['idle']:120
         var percyRenderingStartTime = Date.now()
         waitUntil()
             .interval(2000)
@@ -67,3 +102,4 @@ command.on(
    
 
 });
+

46a43446e6ee57b0e7e8d68b06fb9f96997f3603
maxmattone <max.m@browserstack.com>
2023-03-30 17:23:11 +0100

    use percy build:wait

--- index.js
+++ index.js
@@ -1,22 +1,22 @@
 #!/usr/bin/env node
-const { spawn } = require('node:child_process');
+const spawn = require('cross-spawn');
 const { program } = require('commander');
 var request = require('sync-request');
-var waitUntil = require('wait-until');
-const { finished } = require('node:stream');
+const waitSync = require('wait-sync');
 
 program
     .option('-t, --timeout <number>', 'percysync timeout (seconds)')
     .option('-i, --idle <number>', 'minimum amount of time to wait before stating to poll the Percy API (seconds)')
+    .option('-v, --verbose <boolean>', 'Percy wait verbose ouput', false)
 
 program.parse();
 
 const options = program.opts();
-const command = spawn(program.args[0], program.args.slice(1));
+const command = spawn(program.args[0], program.args.slice(1), { shell: process.env.SHELL });
 const regex = /\[percy\] Finalized build #\d+: https:\/\/percy\.io\/(\w+)\/(?<project_slug>.+)\/builds\/(?<build_id>\d+)/gm;
 var build_id = false;
 var project_slug = false;
-
+var buildStartTime = Date.now()
 
 const median = arr => {
     const mid = Math.floor(arr.length / 2),
@@ -25,7 +25,7 @@ const median = arr => {
   };
 
 command.stdout.on('data', (data) => {
-    console.log(data.toString());
+    console.log(`${data}`);
     let m;
     if(m = regex.exec(data.toString()))
     {
@@ -62,44 +62,30 @@ command.on(
                     {
                         return Date.parse(aBuild['attributes']['finished-at']) - Date.parse(aBuild['attributes']['created-at'])
                     });
-                options['idle'] = median(build_durations)*0.9 / 1000.
-                options['timeout'] = options['timeout']?options['timeout']:(options['idle'] + 60)
+                const testSuiteFinishedTime = Date.now();
+                const testSuiteDuration = testSuiteFinishedTime - buildStartTime;
+                options['idle'] = Math.floor((median(build_durations)*0.9 / 1000) - (testSuiteDuration/1000));
                 console.log("[percysync] We expect the build to take around " + options['idle'] + " seconds");
             }
         }
         options['idle'] = options['idle']?options['idle']:30
         options['timeout'] = options['timeout']?options['timeout']:120
-        var percyRenderingStartTime = Date.now()
-        waitUntil()
-            .interval(2000)
-            .times(Math.floor(options['timeout']*1000/2000))
-            .condition(function() {
-                if(Date.now() - percyRenderingStartTime < options['idle'] * 1000 ){
-                    return false
-                }
-                var res = request('GET', 'https://percy.io/api/v1/builds/'+ build_id, {
-                    headers: { 'Authorization': "Token "+ process.env["PERCY_TOKEN"]},
-                });
+       
+        waitSync(options['idle']); 
 
-                build_status=JSON.parse(res.getBody('utf8'));
-                return (build_status['data']['attributes']['state']!='processing' ? true : false);
-            })
-            .done(function(result) {
-                if(result){
-                    console.log("[percysync] Build state: "+ build_status['data']['attributes']['review-state'])
-                    console.log("[percysync] Snapshot taken: "+ build_status['data']['attributes']['total-snapshots'])
-                    console.log("[percysync] Nb of diff: "+ build_status['data']['attributes']['total-comparisons-diff'])
-                    var percyStatus = (build_status['data']['attributes']['review-state'] == "approved")?0:1
-                    process.exit(code + percyStatus)
-                } else {
-                    console.log("[percysync] Could retrieve Percy build last state.")
-                    process.exit(1)
-                }
-            });
-
+        var percywaitOptions = ["percy", "build:wait", "--fail-on-changes", "--pass-if-approved", "--build", build_id, "--timeout", options['timeout']*1000];
+        if(options['verbose'])
+            percywaitOptions.push("--verbose");
+        const percyWait = spawn("npx", percywaitOptions,  {stdio: "inherit"});
+        percyWait.on('close', (buildCode) => {
+            process.exit(code + buildCode)
+        });
         
     }
-   
+    else
+    {
+        process.exit(code);
+    }
 
 });
 
